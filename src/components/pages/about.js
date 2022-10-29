ef738b41ff686c8557f961c03ca5f10009793555
brettjschmidt <brettjschmidt@gmail.com>
2022-10-29 13:10:41 -0500

    Project completion commit

---
+++ src/components/pages/about.js
@@ -0,0 +1,21 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/ministry-safe.jpg"
+
+export default function() {
+    return (
+        <div className="content-page-wrapper">
+            <div 
+            className="left-column"
+            style={{
+                background: "url(" + profilePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}
+            />
+            <div className="right-column">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Ut lectus arcu bibendum at. Cras pulvinar mattis nunc sed. Dui vivamus arcu felis bibendum ut tristique et egestas. Neque aliquam vestibulum morbi blandit. Donec enim diam vulputate ut pharetra. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget. Feugiat pretium nibh ipsum consequat nisl. Id porta nibh venenatis cras sed. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Pharetra diam sit amet nisl. Purus semper eget duis at tellus.
+
+            </div>
+        </div>
+    );
+}
